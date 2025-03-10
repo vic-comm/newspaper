6a839f3ffcfa9f24160e2b8242a695061005cfdd
Victor Obiezue <obiezuechidera@gmail.com>
2025-03-11 00:31:02 +0100

    permissions_added

--- articles/views.py
+++ articles/views.py
@@ -2,26 +2,39 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from .models import Article
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = 'article_list.html'
     model = Article
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     template_name = 'article_edit.html'
     model = Article
     fields = ('title', 'body')
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = 'article_delete.html'
     model= Article
     success_url = reverse_lazy('article_list')
 
-class ArticleDetailView(DetailView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = 'article_detail.html'
     model = Article
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = 'article_new.html'
     model = Article
     fields = ('title', 'author', 'body')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
